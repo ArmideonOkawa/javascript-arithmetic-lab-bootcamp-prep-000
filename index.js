2c9b6ac895a70678334065bcf916d15b88f9249a
jeremy perry <jeremy.m.perry@gmail.com>
2019-09-24 18:04:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  var num = a + b;
+  return num;
+}
+
+function subtract(a,b){
+  var newNum = a - b;
+  return newNum;
+}
+
+function multiply(a,b){
+  var multi = a * b;
+  return multi;
+}
+
+function divide(a,b){
+  var div = a / b;
+  return div;
+}
+
+function inc(n){
+ n++;
+ return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+parseInt(n, 10);
+  return n;
+}
+
+function preservedDecimal(n){
+  parseFloat(n);
+  return n;
+}
